17e19e9ba8dde72fe2eaa84f2e8dc6c5400d25e8
Dharuan Emina <dharuanluigi@gmail.com>
2022-06-17 20:52:23 -0300

    Added: string extension for sluig

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog6.Extensions
+{
+  public static class StringExtensions
+  {
+    public static string NormalizeSlug(this string slug)
+    {
+      return slug.Replace("@", "-").Replace(".", "-");
+    }
+  }
+}
