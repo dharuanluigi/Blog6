a47308509914f4c3a61b8ac13ed6626b5b8829ce
Dharuan Emina <dharuanluigi@gmail.com>
2022-06-20 10:16:42 -0300

    Added: posts pagination

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog6.ViewModels;
 using Blog6.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,9 +10,11 @@ namespace Blog6.Controllers
   public class PostController : ControllerBase
   {
     [HttpGet("v1/posts")]
-    public async Task<IActionResult> GetAllPostsAsync([FromServices] BlogDataContext context)
+    public async Task<IActionResult> GetAllPostsAsync([FromServices] BlogDataContext context, [FromQuery] int page = 0, [FromQuery] int pageSize = 20)
     {
-      var posts = await context
+      try
+      {
+        var posts = await context
                           .Posts
                           .AsNoTracking()
                           .Include(p => p.Category)
@@ -25,8 +28,31 @@ namespace Blog6.Controllers
                             Category = p.Category.Name,
                             Author = p.Author.Name
                           })
+                          .Skip(page * pageSize)
+                          .Take(pageSize)
+                          .OrderByDescending(p => p.LastUpdateDate)
                           .ToListAsync();
-      return Ok(posts);
+
+        if (posts is null || posts.Count == 0)
+        {
+          return NotFound(new ResultViewModel<string>("No posts was found"));
+        }
+
+        var result = new ResultPostsViewModel
+        {
+          Total = posts.Count,
+          Page = page,
+          PageSize = pageSize,
+          Posts = posts
+        };
+
+        return Ok(new ResultViewModel<ResultPostsViewModel>(result, null));
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        return StatusCode(500, new ResultViewModel<string>("Ocurrend an internal server error when try to retrive all posts"));
+      }
     }
   }
 }
---
+++ ViewModels/Posts/ResultPostsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog6.ViewModels.Posts
+{
+  public class ResultPostsViewModel
+  {
+    public int Total { get; set; }
+
+    public int Page { get; set; }
+
+    public int PageSize { get; set; }
+
+    public IList<ListPostsViewModel> Posts { get; set; }
+  }
+}
